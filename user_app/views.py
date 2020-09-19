9bd92b65f0930f5b0afb41607ed20a5b75a3caa6
SwikarGautam <swikar.gautam@gmail.com>
2020-09-19 13:31:37 +0545

    [Issue 6] Create a route api/v1/donors for listing donors

--- user_app/views.py
+++ user_app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from user_app.serializers import UserSerializer
+from user_app.serializers import UserSerializer, DonorUserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import CustomUser
 
 
 class UserDetailView(APIView):
 
     def get(self, request, format=None):
         return Response(UserSerializer(request.user).data)
+
+
+class DonorListView(APIView):
+
+    def get(self, request, format=None):
+        queryset = CustomUser.objects.filter(wants_to_donate=True)
+        serializer = DonorUserSerializer(queryset, many=True)
+        return Response(serializer.data) 
 
