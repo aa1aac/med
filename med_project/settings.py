91b486dff1c15df80c6053c127cc068c19fc7fb7
SwikarGautam <swikar.gautam@gmail.com>
2020-09-19 08:19:21 +0545

    Create frontend_app

--- med_project/settings.py
+++ med_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local
+    'frontend_app.apps.FrontendAppConfig',
 ]
 
 MIDDLEWARE = [
