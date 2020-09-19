1afe7329fcb8f5514c9b834fe5f43aaa37fa44b3
SwikarGautam <swikar.gautam@gmail.com>
2020-09-19 19:54:40 +0545

    [Issue 15] Create a route /api/v1/user/update for updating user info

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from .views import UserDetailView
+from .views import UserDetailView, UpdateUserDetailView
 
 urlpatterns = [
     path('signup/', include('rest_auth.registration.urls')),
     path('', include('rest_auth.urls')),
     path('me/', UserDetailView.as_view()),
+    path('update', UpdateUserDetailView.as_view()),
 ]
 
--- user_app/views.py
+++ user_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from user_app.serializers import UserSerializer, DonorUserSerializer
+from user_app.serializers import UserSerializer, DonorUserSerializer, UserUpdateSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions, status
 from .models import CustomUser
 
 
@@ -27,8 +27,21 @@ class CompatibleDonorListView(APIView):
 
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, requests, blood_group, format=None):
+    def get(self, request, blood_group, format=None):
         queryset = CustomUser.objects.filter(blood_group=blood_group)
+        queryset = queryset.objects.filter(wants_to_donate=True)
         serializer = DonorUserSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class UpdateUserDetailView(APIView):
+
+    def put(self, request):
+        serializer = UserUpdateSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
