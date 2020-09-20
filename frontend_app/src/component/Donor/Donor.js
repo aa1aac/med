9bc0b0fe35ea082192e21783cad95a7942a8e877
aa1aac <35446762+aa1aac@users.noreply.github.com>
2020-09-20 09:03:22 +0545

    [ISSUE 21] Add some UI changes and color changes

--- frontend_app/src/component/Donor/Donor.js
+++ frontend_app/src/component/Donor/Donor.js
@@ -3,13 +3,15 @@ import React from "react";
 const Donor = ({ donor }) => {
   return (
     <div
-      className="card text-white bg-dark m-3 "
-      style={{ maxWidth: "19rem" }}
+      className="card text-white bg-primary m-3 "
+      // style={{ maxWidth: "19rem" }}
       key={donor.email}
     >
-      <div className="card-header">{donor.full_name}</div>
+      <div className="card-header">
+        <h4> Blood Group : {donor.blood_group}</h4>
+      </div>
       <div className="card-body">
-        <h5 className="card-title"> Blood Group : {donor.blood_group}</h5>
+        <h5 className="card-title">{donor.full_name}</h5>
         <a
           className="btn btn-secondary text-dark"
           href={`mailto:${donor.email}`}
