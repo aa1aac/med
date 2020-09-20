110abaa8bccad4c2bf6a7b137e796da9bc38d740
aa1aac <35446762+aa1aac@users.noreply.github.com>
2020-09-20 10:02:31 +0545

    [ISSUE 25] display queried blood group

--- frontend_app/src/component/Donor/Donor.js
+++ frontend_app/src/component/Donor/Donor.js
@@ -2,11 +2,7 @@ import React from "react";
 
 const Donor = ({ donor }) => {
   return (
-    <div
-      className="card text-white bg-primary m-3 "
-      // style={{ maxWidth: "19rem" }}
-      key={donor.email}
-    >
+    <div className="card text-white bg-primary m-3 " key={donor.email}>
       <div className="card-header">
         <h4> Blood Group : {donor.blood_group}</h4>
       </div>
--- frontend_app/src/pages/home.js
+++ frontend_app/src/pages/home.js
@@ -5,6 +5,7 @@ import Donor from "../component/Donor/Donor";
 
 const home = () => {
   const [donors, setDonors] = useState([]);
+  const [bloodGroup, setBloodGroup] = useState(" ");
 
   useEffect(() => {
     fetchDonors();
@@ -16,8 +17,42 @@ const home = () => {
     });
   };
 
+  const fetchByGroup = () => {
+    setDonors([]);
+    axios
+      .get(`/api/v1/donors/${bloodGroup}`)
+      .then((res) => {
+        setDonors(res.data);
+      })
+      .catch((e) => {
+        console.error(e);
+        toast.error("Uh Oh! some error occured fetching the data");
+      });
+  };
+
   return (
     <div className="container mt-5">
+      <div className="form-group">
+        <label htmlFor="bloodGroup">Search by Blood group</label>
+        <select
+          className="form-control"
+          id="bloodGroup"
+          value={bloodGroup ? bloodGroup : " "}
+          onChange={(e) => {
+            setBloodGroup(e.target.value);
+            fetchByGroup();
+          }}
+        >
+          <option value="A+">A+</option>
+          <option value="B+">B+</option>
+          <option value="O+">O+</option>
+          <option value="AB+">AB+</option>
+          <option value="A-">A-</option>
+          <option value="B-">B-</option>
+          <option value="O-">O-</option>
+          <option value="AB-">AB-</option>
+        </select>
+      </div>
       <div className="row">
         {donors.map((donor) => (
           <Donor donor={donor} key={donor.email} />
